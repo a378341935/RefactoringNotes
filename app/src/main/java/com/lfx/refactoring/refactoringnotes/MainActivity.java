56874da76b0e557a9eb7086233878f9ceecb924d
a378341935 <vigneliufx@163.com>
2016-08-23 16:29:00 +0800

    First Edit

---
+++ app/src/main/java/com/lfx/refactoring/refactoringnotes/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lfx.refactoring.refactoringnotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 重构Refactoring: 在不改变代码外在行为的前提下，对代码做出修改，以改进程序的内部结构。
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
